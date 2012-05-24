d1160e860004bbb3a105134c7644137e01429207
Stephen Tudor <smt@sunblu.sh>
2012-05-24 16:47:29 -0400

    Use a closure

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
-var metas = document.getElementsByTagName('meta');
-var i;
-function gestureStart() {
-  for (i=0; i<metas.length; i++) {
-    if (metas[i].name === "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
+(function (d) {
+  var metas = d.getElementsByTagName('meta');
+  var i;
+  function gestureStart() {
+    for (i=0; i<metas.length; i++) {
+      if (metas[i].name === "viewport") {
+        metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
+      }
     }
   }
-}
-if (navigator.userAgent.match(/iPhone/i)) {
-  for (i=0; i<metas.length; i++) {
-    if (metas[i].name === "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+  if (navigator.userAgent.match(/iPhone/i)) {
+    for (i=0; i<metas.length; i++) {
+      if (metas[i].name === "viewport") {
+        metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+      }
     }
+    d.addEventListener("gesturestart", gestureStart, false);
   }
-  document.addEventListener("gesturestart", gestureStart, false);
-}
+}(document));
